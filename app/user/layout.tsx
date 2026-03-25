53e69f635708b3829a332756ded143fc8872fda9
Abhiram-Gaddam <gaddamabhiram.53@gmail.com>
2026-03-25 19:52:48 +0530

    mobile version

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,24 +1,4 @@
-// // User layout.tsx (app/user/layout.tsx)
-// import { Sidebar } from "@/app/user/components/UserSidebar";
-// import { Navbar  } from "@/app/user/components/UserNavbar";
-
-// export default function UserLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <>
-//       <Sidebar />
-//       <div className="flex-1 ml-[260px] flex flex-col min-h-screen pt-[76px]">
-//         <Navbar />
-//         <main className="p-6 pb-12 flex-1 w-full max-w-[1440px] mx-auto bg-[#FFFFFF]">
-//           {children}
-//         </main>
-//       </div>
-//     </>
-//   );
-// }
+ 
 // User layout.tsx (app/user/layout.tsx)
 import { Sidebar } from "@/app/user/components/UserSidebar";
 import { Navbar } from "@/app/user/components/UserNavbar";
@@ -30,8 +10,29 @@ export default function UserLayout({
 }>) {
   return (
     <>
-      <Sidebar />
-      <div className="flex-1 ml-[260px] flex flex-col min-h-screen pt-[76px]">
+    <style>{`
+        .sidebar-wrapper {
+          display: none;
+        }
+
+        .main-content {
+          margin-left: 0;
+        }
+
+        @media (min-width: 768px) {
+          .sidebar-wrapper {
+            display: block;
+          }
+
+          .main-content {
+            margin-left: 260px;
+          }
+        }
+      `}</style>
+
+    <div className="sidebar-wrapper" >
+      <Sidebar /></div>
+      <div className="flex-1 main-content flex flex-col min-h-screen pt-[76px]">
         <Navbar />
         <main className="flex-1 w-full bg-[#FFFFFF]">
           {children}
@@ -39,4 +40,5 @@ export default function UserLayout({
       </div>
     </>
   );
-}
+} 
+ 
